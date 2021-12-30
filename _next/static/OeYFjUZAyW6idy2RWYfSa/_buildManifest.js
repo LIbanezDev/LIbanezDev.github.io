f04be2a8df6dd5376620216feb08980fa463b064
LIbanezDev <lucas.vergara@usm.cl>
2021-12-30 04:12:05 +0000

    Deploying to gh-pages from  @ 80a3657170dcba1dfec64b77acb2c8700139c6c1 🚀

---
+++ _next/static/OeYFjUZAyW6idy2RWYfSa/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-309b10b560a164d33b28.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
